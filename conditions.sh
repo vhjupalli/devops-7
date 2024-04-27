f40badd31391cd14b2c5e848fd3ab6b04c99a423
vinodjupalli <vinodhari7jupalli@gmail.com>
2024-04-27 20:44:50 +0530

    qqqq

--- conditions.sh
+++ conditions.sh
@@ -4,14 +4,14 @@ NUM1=$1
 NUM2=$2
 
 
-if [NUM1 -gt NUM2]
+if [$NUM1 -gt 20]
 
   then
 
-  echo "$NUM1 is greater than ${NUM2}"
+  echo "$NUM1 is greater than 20"
   
   else
 
-   echo "$NUM1 is less than ${NUM2}"
+   echo "$NUM1 is less than 20"
 
    fi
